0564866ec70ce2d3c2c43f78f7a9d858852fa5b9
Rashad Muntar <muntartoure@gmail.com>
2024-04-28 10:37:31 +0000

    add db connection

---
+++ config/db.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"os"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(){
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	DB = db
+}
